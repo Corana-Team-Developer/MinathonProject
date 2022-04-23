792ea087903aa236f40e523e80ab8cd0cc203ae8
ChienNguyenDac <chien.nguyen@enpii.com>
2022-04-23 14:50:36 +0700

    Initial User

---
+++ server/src/model/merchant.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+import { FITNESS_GOAL } from "./customer.model.js"
+
+const MerchantSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    CV: String,
+    advantages: [
+        {
+            type: Number,
+            enum: [FITNESS_GOAL.GAIN_WEIGHT, FITNESS_GOAL.LOSE_WEIGHT, FITNESS_GOAL.MAINTAIN]
+        }
+    ],
+    addressWork: {
+        detail: String,
+        code: Number
+    }
+})
+
+const Merchant = mongoose.model('Merchant', MerchantSchema)
+export default Merchant
