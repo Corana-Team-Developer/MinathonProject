43d95392c1973ebec5663ef995c029ebbdceb5f5
Trieu Phi <phi.trantrieu@hcmut.edu.vn>
2022-04-23 14:59:11 +0700

    create admin route

--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -1,8 +1,12 @@
-import { Router } from "express";
+import { Router } from "express"
 import exerciseRoute  from "./exercise.route.js"
-import categoryRoute from "./category.route"
+import categoryRoute from "./category.route.js"
+import checkUserRole from "../middleware/checkUserRole.js"
+import { UserType } from "../model/user.model.js"
+
 const admin = Router()
 
+admin.use(checkUserRole([UserType.ADMIN]))
 admin.use('/exercise', exerciseRoute)
 admin.use('/category', categoryRoute)
 
