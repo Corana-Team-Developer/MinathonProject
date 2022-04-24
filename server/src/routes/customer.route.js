d24ed13db29dfd4d83a9f2eb5794eaea03f67b57
TheEleven11 <tanphatld3@gmail.com>
2022-04-24 09:10:12 +0700

    create feedback route

--- server/src/routes/customer.route.js
+++ server/src/routes/customer.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express"
-import { updateWorkoutProfile, updateWorkoutGoal, appendWorkoutProgress, appendFoodToFoodTracking, editFoodFromFoodTracking, deleteFoodFromFoodTracking, getFoodTracking } from "../controller/customer.controller.js"
+import { updateWorkoutProfile, updateWorkoutGoal, appendWorkoutProgress, appendFoodToFoodTracking, editFoodFromFoodTracking, deleteFoodFromFoodTracking, getFoodTracking,
+    createFeedback, updateFeedback, deleteFeedback, getMerchantFeedback, showFeedback } from "../controller/customer.controller.js"
 import checkUserRole from "../middleware/checkUserRole.js"
 import { UserType } from "../model/user.model.js"
 
@@ -13,4 +14,9 @@ router.get('/food', getFoodTracking)
 router.post('/food/append', appendFoodToFoodTracking)
 router.post('/food/edit', editFoodFromFoodTracking)
 router.post('/food/delete', deleteFoodFromFoodTracking)
+router.post('/feedback/create', createFeedback)
+router.post('/feedback/update', updateFeedback)
+router.post('/feedback/delete', deleteFeedback)
+router.get('/feedback/merchant/:merchantId', getMerchantFeedback)
+router.get('/feedback/:feedbackId', showFeedback)
 export default router
